2afc20846ddfcb0011a0a7140fa76fe5592dd1ce
BuildTools <unconfigured@null.spigotmc.org>
2019-11-06 22:27:58 +0100

    QuaternionTesting
    Played around with quaternions and will be implementing methods with them soon

---
+++ Assets/Scripts/BoxRayCollider3D.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider))]
+public class BoxRayCollider3D : RayCollider3D
+{
+    new BoxCollider collider;
+    [SerializeField]
+    Vector3 directionTest = Vector3.forward;
+    [SerializeField]
+    Vector3 gravityDirTest = Vector3.up;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        collider = GetComponent<BoxCollider>();
+        Vector3 pos = collider.center;
+        Matrix4x4 matrix = transform.localToWorldMatrix;
+        Vector3 actualPos = matrix.MultiplyPoint3x4(pos);
+        Vector3 direction = matrix.MultiplyVector(Vector3.down);
+        matrix = transform.worldToLocalMatrix;
+        actualPos = matrix.MultiplyPoint3x4(pos);
+    }
+    private void LateUpdate()
+    {
+        Quaternion q = transform.localRotation;
+        q.SetLookRotation(directionTest,gravityDirTest);
+        transform.localRotation = q;
+    }
+
+}
---
+++ Assets/Scripts/RayCollider3D.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public abstract class RayCollider3D : MonoBehaviour
+{
+    [SerializeField]
+    float skinWidth = 0.3f;
+    List<Vector3> RayCastOrigin = new List<Vector3>();
+    public Vector3 GravityDir { 
+        get { return gravityDir; } 
+        set { gravityDir = value == null ? gravityDir : value.normalized; } 
+    }
+    private Vector3 gravityDir = Vector3.down;
+    public int GravityStrength = 10;
+    
+    
+}
